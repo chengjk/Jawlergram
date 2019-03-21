5158acfe33b25d2e6486aafa036d9a15393aa17d
jacky <jacky.cheng@yeahmobi.com>
2019-03-21 14:59:59 +0800

    update cmd

---
+++ src/main/java/com/crawlergram/crawler/CliCmdUtil.java
@@ -0,0 +1,22 @@
+/*
+ * Title: CliCmdUtil.java
+ * Project: Jawlergram
+ * Creator: Georgii Mikriukov
+ * 2019
+ */
+
+package com.crawlergram.crawler.output;
+
+/**
+ * created by jacky. 2019/3/21 2:42 PM
+ */
+public class CliCmdUtil {
+
+    public static final String o = "operate";
+    public static final String sc = "sourceChannel";
+    public static final String tc = "targetChannel";
+    public static final String file = "file";
+
+
+
+}
